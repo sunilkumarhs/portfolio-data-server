12401310cceff5a38ba522ae59ab9c20674b4dc9
sunilkumarhs <sunilkumarhs974117@gmail.com>
2024-09-20 13:30:02 +0530

    vercel deploy 1

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+
 server.use(router);
 
 server.listen(port);
